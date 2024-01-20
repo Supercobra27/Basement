37e77b603d1c447e127c97bb789ab6cf5b2e72a7
Supercobra27 <silverbergryan@gmail.com>
2024-01-20 18:39:00 -0500

    added cpp, or zig

---
+++ .cpp (C++)/hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int number;
+    cout << "Hello, World!";
+    cin >> number;
+    cout << "You chose: " << number;
+
+    return 0;
+}
---
+++ .s (Assembly)/registers.md
@@ -0,0 +1,11 @@
+### Registers
+#### Data
+- AX (Accumulator)
+- BX (Base)
+- CX (Count)
+- DX (Data)
+
+#### Pointer
+- IP (Instruction)
+- SP (Stack)
+- BP (Base)
