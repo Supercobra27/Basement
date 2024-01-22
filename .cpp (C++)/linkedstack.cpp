ab21d556276673a31d71e04aaf9c5bf2e3fe71b8
Supercobra27 <silverbergryan@gmail.com>
2024-01-22 10:14:23 -0500

    linkedstack

--- .cpp (C++)/linkedstack.cpp
+++ .cpp (C++)/linkedstack.cpp
@@ -4,17 +4,37 @@
 
 using namespace std;
 
-typedef struct stack *stk_ptr;
+typedef struct stack *stk_ptr; //Pointer to the head of the stack
 
-typedef struct stack{
+typedef struct stack{ //stack containing the head pointer
     struct node* head;
 }stack;
 
-struct node{
+struct node{ //nodes for stack
     int value;
     struct node* next;
 
 }node;
+
+stk_ptr stack_new() { 
+    stk_ptr s = new stack;
+    s->head = NULL;
+    return s;
+}
+
+void stack_free(stk_ptr s){
+    struct node* curr;
+    while(curr != NULL){
+        if(s->head->next == NULL){
+            free(s->head);
+        }else{
+        curr = s->head;
+        s->head = s->head->next;
+        free(curr);
+        }
+    }
+    free(s);
+}
 
 void push(stk_ptr s, int value){ //pushes an item onto the stack
     struct node *newNode = new struct node;
@@ -24,12 +44,24 @@ void push(stk_ptr s, int value){ //pushes an item onto the stack
 }
 
 int pop(stk_ptr s){
-    return s->head->value;
+    struct node *top = s->head;
+    s->head = s->head->next;
+    int value = top->value;
+    free(top);
+
+    return value;
 }
 
 
 
 int main(){
+    stk_ptr s = stack_new();
+    push(s,2);
+    push(s,3);
+    printf("%d\n",pop(s));
+    push(s,4);
+    printf("%d\n",pop(s));
+    stack_free(s);
 
     return 0;
 }
