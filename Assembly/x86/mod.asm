e17f89fedd11eb311f18324fc32f0a04b8f32e69
Supercobra27 <silverbergryan@gmail.com>
2024-02-15 23:57:46 -0500

    changed to .asm

---
+++ Assembly/x86/mod.asm
@@ -0,0 +1,17 @@
+; Author: Ryan Silverberg
+; Purpose: To compute modular arithmetic on two numbers
+
+.text
+
+main:
+    push rbp        ; Push onto the stack
+    mov rbp, rsp    ; Move the stack pointer onto the base pointer
+    mov DW PTR [rbp-4], 4567
+    mov DW PTR [rbp-8], 123
+    mov rax, DW PTR [rbp-4]
+    cdq
+    idiv DW PTR [rbp-12]
+    mov DW PTR [rbp-12], rdx
+    mov rax, 0
+    pop rbp
+    ret
--- Assembly/x86/mod.s
+++
@@ -1,3 +0,0 @@
-# Author: Ryan Silverberg
-# Purpose: Modular Arithmetic done in x86 Assembly
-
